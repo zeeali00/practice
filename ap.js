e006c9f85a539b0378b46de602e2c7e498997d30
Zeeshan Ali <zee.ali00@yahoo.com>
2018-02-23 22:00:26 +0500

    Assignment

---
+++ ap.js
@@ -0,0 +1,155 @@
+//Task 1
+//1.	Write a script to greet your website visitor using JS alert box.
+//alert("Welcome to Our Webpage You can find any thing You want here!");
+
+//Task 2
+//2.	Write a script to display following message on your web page:
+//alert("Error!Enter The Valid Password");
+
+//Task 3
+//3.	Write a script to display following message on your web page: (Hint : Use line break);
+//alert("Welcome to js Land...\n HappyCoding!");
+
+//Task 4
+//4.	Write a script to display following messages in sequence:
+ 
+ //alert("Welcome to Js Land");
+ //alert("HappyCoding!");
+
+ //Task 5
+ //1. Declare a variable called username. 
+ //2. Declare a variable called myName & assign to it a string that represents your Full Name. 
+ //3. Write a script to  a. declare a JS variable, titled message  b. assign “Hello World” to the variable message c. display the message in alert box 
+ //4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. Example: 
+
+//  var username;
+//  var myName="Zeeshan Ali";
+//  var message="HelloWorld";
+//  alert(message);
+// var name="Zeeshan Ali";
+// var Rollnum="16386";
+// var course="Web and MobileApp Development";
+// alert(name+"\n"+Rollnum+"\n"+course);
+
+//Task 6
+// var a="Pizza\nPizz\nPiz\nPi\nP";
+// alert(a);
+
+//Task7
+// number of children, partner’s name, geographic location, and job title
+// var n=5;
+// var pname="Hahahahha";
+// var gpl="Pakistan";
+// var job="CEO of Your own Company";
+// alert("Welcome to Fortune Teller Baba Asad Bangali");
+// alert("Number of Childrens: "+n+"\nYour partner Name: "+pname+"\nYour location: "+gpl+"\nYour job title: "+job);
+
+//Task 8
+// Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com). Show the below mentioned message in an alert box. (Hint: use string concatenation) 
+// var email;
+// email="zee.ali00@yahoo.com";
+// alert("My Email Address is: "+email);
+
+//Task 9
+//Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box: 
+// var book;
+// book="A smarter way to learn JavaScript";
+// alert("I am Trying Learning From The Book: "+book);
+
+//Task 10
+//Write a script to display this in browser through JS 
+// document.write("Ya!I can Write Code in JavaScript");
+
+//Task 11
+// Declare a variable called age & assign to it your age. Show your age in an alert box. 
+// var age=19;
+// alert("I am"+age+"Years Old");
+
+//Task 12
+// Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”. 
+// var a="You have visited 21 times"
+//alert(a);
+
+//Task 13
+// Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser
+// var birthYear="1998";
+// document.write("My Birth Year is: "+birthYear);
+
+//Task 14
+// A visitor visits an online clothing store www.xyzClothing.com .
+// Write a script to store in variables the following information:  a. Visitor’s name b. Product title c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.  
+ 
+//  var name="Johny Papa";
+//  var pTilte="T-Shirt"
+//  var Quantity=5;
+//  document.write(name+pTilte+Quantity);
+
+//Task 15
+// Write a program that take two numbers & add them in a new variable. Show the result in your browser. 
+// var a=+prompt("Enter 1st Number");
+// var b=+prompt("Enter 2nd Number");
+// var c= a+b;
+// alert("The sum of "+a+" and "+b+" is: "+c);
+
+//Task 16
+ //Repeat task1 for subtraction, multiplication, division & modulus. E.g. 
+// var a=+prompt("Enter 1st Number");
+// var b=+prompt("Enter 2nd Number");
+// var c= a-b;
+// alert("The subtraction of "+a+" and "+b+" is: "+c);
+// var a=+prompt("Enter 1st Number");
+// var b=+prompt("Enter 2nd Number");
+// var c= a*b;
+// alert("The multiplication of "+a+" and "+b+" is: "+c);
+// var a=+prompt("Enter 1st Number");
+// var b=+prompt("Enter 2nd Number");
+// var c= a/b;
+// alert("The division of "+a+" and "+b+" is: "+c);
+
+//Task 17
+// Declare a variable.  b. Show the value of variable in your browser like “Value after variable declaration is: ??”. 
+//c. Initialize the variable with some number. d. Show the value of variable in your browser like “Initial value: 3”. e. Increment the variable. f. Show the value of variable in your browser like “Value after increment is: 4”. g. Add 7 to the variable. h. Show the value of variable in your browser like “Value after addition is: 11”.  i. Decrement the variable. j. Show the value of variable in your browser like “Value after decrement is: 10”.
+// k. Show the remainder after dividing the variable’s value by 3. Output : “The remainder is : 1” 
+ 
+// var a;
+// document.write("Value of variable after Decleration:  "+a);
+// a=3;
+// document.write("Value of variable after Initilization:  "+a);
+// a++;
+// document.write("Value of variable after Increment:  "+a);
+// a=a+7;
+// document.write("Value of variable after 7 addition:  "+a);
+// a--;
+// document.write("Value of variable after Decrement:  "+a);
+// a=a%3;
+// document.write("Value of variable after division:  "+a);
+
+//Task 18
+// var cost=600;
+// var t=cost*5;
+// document.write("Total Cost of 5 Tickets is: "+t+" PKR")
+
+
+//Task 19
+//
+// var a=+prompt("Enter The Number");
+// document.write("multiplication table of  "+a);
+// document.write("</br>");
+// for(var i=1;i<=10;i++){
+// var t=a*i;
+// document.write(a+" x "+i+ " ="+t)
+// document.write("</br>");
+// }
+
+//Task 20
+//  The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here. a. Store a Celsius temperature into a variable. b. Convert it to Fahrenheit & output “NNoC is NNoF”. 
+//c. Now store a Fahrenheit temperature into a variable. d. Convert it to Celsius & output “NNoF is NNoC”. 
+// var c=32;
+// var f=75;
+// var g=(f-32)*5/9;
+// var h=(c*9/5)+32;
+// document.write(c+"<sup>o</sup>C is "+g);
+// document.write(c+"<sup>f</sup>C is "+h);
+
+
+
